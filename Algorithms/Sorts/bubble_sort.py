b4d14775f94eae78eba15c238e219dabf85e3783
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-11 03:53:21 +0600

    Bubble Sort

--- Algorithms/Sorts/bubble_sort.py
+++ Algorithms/Sorts/bubble_sort.py
@@ -1,1 +1,26 @@
+# Bubble sort in Python
+
+def bubbleSort(array):
+    length = len(array)
+
+    for i in range(length - 1):
+        flag = False
+        #  Last i elements are already in place
+        for j in range(length - i - 1):
+
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                flag = True
+
+        if flag == False:
+            break
+
+
+data = [1, 2, 3, 4, 5]
+
+bubbleSort(data)
+print('Sorted Array in Ascending Order:')
+print(data)
 

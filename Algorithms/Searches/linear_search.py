0caeca8cc2a7c95c3f917eac7b245de7b14e35c5
Maruf <marufur1847@gmail.com>
2021-07-16 14:53:54 +0600

    Linear search

--- Algorithms/Searches/linear_search.py
+++ Algorithms/Searches/linear_search.py
@@ -1,1 +1,15 @@
+def linearsearch(values, item):
+    for i in range(len(values)):
+        if values[i] == item:
+            return i
 
+    return -1
+
+
+values = [1, 4, 2, 6, 8, 10, 12, 16, 77, 43, 55, 100, 98]
+item = 77
+result = linearsearch(values, item)
+if (result == -1):
+    print("Element is not present in array")
+else:
+    print("Element is present at index", result)
